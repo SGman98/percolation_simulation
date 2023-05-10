5e2c46a99f152dc301112754bfe61c5994e530b3
SGman98 <bastiangarnica@gmail.com>
2023-05-10 17:40:43 -0500

    feat: add function to create a matrix
    matrix has random holes depending on `p`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rand::prelude::*;
+
+fn create_matrix(n: usize, m: usize, p: f64) -> Vec<Vec<u8>> {
+    let mut rng = rand::thread_rng();
+    let mut matrix = vec![vec![0; m]; n];
+    for i in 0..n {
+        for j in 0..m {
+            matrix[i][j] = if rng.gen::<f64>() < p { 1 } else { 0 };
+        }
+    }
+    matrix
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10;
+    let m = 10;
+    let p = 0.5;
+    let matrix = create_matrix(n, m, p);
+    for i in 0..n {
+        for j in 0..m {
+            print!("{}", matrix[i][j]);
+        }
+        println!();
+    }
 }
